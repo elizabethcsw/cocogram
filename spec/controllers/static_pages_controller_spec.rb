a5b2a216e633efdb77e2f1cb50ec0c891b8bc56b
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-09-24 00:51:33 +0100

    before bootstrap

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+
+RSpec.describe StaticPagesController, type: :controller do
+  render_views
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+      # response.should have_selector("title", :content =>"Home | Cocogram")
+      # response.should have_selector("title", :text =>"Home | Cocogram")
+    end
+  end
+
+  describe "GET #help" do
+    it "returns http success" do
+      get :help
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.feature "Static pages feature tests", :type => :feature do
+
+  base_title = "Cocogram"
+
+  scenario "Visiting the home page" do
+    visit "/static_pages/home"
+    # expect(page).to have_title "Home | Cocogram"
+    expect(page.title).to eq "#{base_title}"
+  end
+
+  scenario "Visiting the about page" do
+    visit "/static_pages/about"
+    expect(page).to have_title "About | #{base_title}"
+    # expect(page.title).to eq "Home | Cocogram"
+  end
+
+  scenario "Visiting the help page" do
+    visit "/static_pages/help"
+    expect(page).to have_title "Help | #{base_title}"
+    # expect(page.title).to eq "Home | Cocogram"
+  end
+
+  scenario "Visiting the contact page" do
+    visit "/static_pages/contact"
+    expect(page).to have_title "Contact | #{base_title}"
+    # expect(page.title).to eq "Home | Cocogram"
+  end
+end
